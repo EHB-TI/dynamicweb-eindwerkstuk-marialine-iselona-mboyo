d09050d49bcc2d9265b8fe0510d53e749cc02cf7
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-04-01 21:58:36 +0200

    javascript login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Het werkt");
+console.log("It's working!!");
+
+//Create a user account
+
+//Login system
+let users = [
+    {
+        username: "lina",
+        password: "loops123"
+    },
+    {
+        username: "john",
+        password: "academy10"
+    }
+]
+
+function loggedIn() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    console.log("you're username is: " + username + " and your password is " + password);
+
+    for(i = 0; i < users.length; i++){
+        if (username == users[i].username && password == users[i].password) {
+            console.log(username + " is logged in.");
+            return
+        }
+    }
+    console.log("Username or password is incorrect");
+}
+
+console.log(loggedIn);
