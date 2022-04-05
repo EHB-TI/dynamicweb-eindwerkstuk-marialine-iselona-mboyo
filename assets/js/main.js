fe62da9df2412887e1b88a537c65f31d2ef6285d
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-04-05 22:14:09 +0200

    JavaScript changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ console.log("It's working!");
 //Quotes
 
 // //Getting the quote, author & button of next quote
-// let quote = document.getElementsByClassName("quote-text");
-// let author = document.getElementsByClassName("author");
-// let newQuoteBtn = document.getElementsByClassName("btn-click-newQuote");
+let quote = document.getElementsById("text");
+let author = document.getElementsById("author");
+let newQuoteBtn = document.getElementsByClassName("btn-click-newQuote");
 
 // //Adding API link
 // const url = 'https://type.fit/api/quotes';
@@ -19,18 +19,39 @@ console.log("It's working!");
 //     });
 // };
 
-// window.addEventListener("load", getQuote);
-// newQuoteBtn.addEventListener("click", getQuote)
+//window.addEventListener("load", getQuote);
+ //newQuoteBtn.addEventListener("click", getQuote)
+
+async function getQuote() {
+    const response = await fetch("https://type.fit/api/quotes")
+    const data = await response.json();
+
+console.log(data);
+
+}
+
+getQuote()
+
 
 
 
 fetch('https://type.fit/api/quotes')
-.then(res => {
-    if (res.ok) {
-        console.log('SUCCESS');
-    } else {
-        console.log("Not Successful");
-    }
+ .then(res => {
+     if (res.ok) {
+         console.log('SUCCESS');
+     } else {
+         console.log("Not Successful");
+     }
+ })
+ .then(data => console.log(data))
+ .catch(error => console.log('ERROR'))
+
+
+
+fetch('https://type.fit/api/quotes')
+.then(data => {
+return data.json();
 })
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
+.then(post => {
+console.log(post.title);
+});
